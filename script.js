c924704e52181f17073eaba0e1917cb4e9a60c95
Olha Sotova <sotovaola21@gmail.com>
2024-02-17 09:41:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// Подопытные данные
+
+// Массив чисел (примитивы)
+const numbers = [1,3,6,2,4,0,9,7,5,10,15,12,11,9,2,7];
+
+
+
+
+// Массив строк (примитивы)
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+
+const ball = {
+    name: "Ball",
+    category: "Sport",
+    img: "./assets/ball.jpeg",
+    price: 100
+};
+const gloves = {
+    name: "Gloves",
+    category: "Sport",
+    img: "./assets/gloves.jpg",
+    price: 140
+};
+const shoes = {
+    name: "Sport shoes",
+    category: "Sport",
+    discount: true,
+    img: "./assets/shoes.png",
+    price: 320
+};
+const hammer = {
+    name: "Hammer",
+    category: "Tools",
+    img: "./assets/hammer.jpeg",
+    price: 40
+};
+const saw = {
+    name: "Saw",
+    category: "Tools",
+    discount: true,
+    img: "./assets/saw.jpeg",
+    price: 75
+};
+const shark = {
+    name: "Shark toy",
+    category: "Other",
+    img: "./assets/shark.jpeg",
+    price: 45
+};
+
+// Массив объектов
+let products = [ball, gloves, shoes, hammer, saw];
+
+const people = [
+    { name: 'John', age: 28 },
+    { name: 'Jane', age: 32 },
+    { name: 'Peter', age: 45 },
+    { name: 'Mark', age: 20 },
+    { name: 'Maria', age: 30 },
+    { name: 'Paul', age: 62 },
+    { name: 'Anna', age: 26 },
+    { name: 'Samantha', age: 51 },
+    { name: 'Leonard', age: 39 },
+    { name: 'Luisa', age: 55 }
+];
+
+const words = ["JavaScript", "HTML", "CSS", "React"];
+
+const numbers2 = [1, 2, 3, 4, 5];
